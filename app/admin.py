2cc4499336c9b33f31727f4e3a56af250ee78707
pratik213 <dahalp30@gmail.com>
2023-03-13 15:56:24 +0545

    Completed admin and model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import(
+    Customer,
+    Product,
+    Cart,
+    OrderPlaced
+)
+
+@admin.register(Customer)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','name','locality','city','zipcode','province']
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    # pass
+    list_display=['id','title','selling_price','description','discounted_price','brand','category','product_img']
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display=['id','user','product','quantity']
+
+@admin.register(OrderPlaced)
+class OrderPlacedModelAdmin(admin.ModelAdmin):
+    pass
+    list_display=['id','user','customer','product','ordered_data','quantity','status']
--- app/models.py
+++ app/models.py
@@ -18,10 +18,57 @@ class Customer(models.Model):
     locality=models.CharField(max_length=200)
     city=models.CharField(max_length=200)
     zipcode=models.IntegerField()
-    state=models.CharField(choices=PROVINCE_CHOICES,max_length=50)
+    province=models.CharField(choices=PROVINCE_CHOICES,max_length=50)
 
     def __str__(self):
         return str(self.id)
-        
+
+CATEGORY_CHOICES=(
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW','Bottom Wear'),
+)
+
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discounted_price=models.IntegerField()
+    description=models.TextField(blank=True)
+    brand=models.CharField(max_length=100)
+    category=models.CharField(choices=CATEGORY_CHOICES,max_length=2)
+    product_img=models.ImageField(upload_to='productimg')
+
+    def __str__(self):
+        return str(self.id)
+    
+
+class Cart(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+STATUS_CHOICES=(
+    ('ACCEPTED','ACCEPTED'),
+    ('PACKED','PACKED'),
+    ('ON THE WAY','ON THE WAY'),
+    ('DELIVERED','DELIVERED'),
+    ('CANCEL','CANCEL'),
+)
+
+class OrderPlaced(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    ordered_data=models.DateField(auto_now_add=True)
+    status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+
+
+
+
     
 
