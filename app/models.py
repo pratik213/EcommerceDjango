d0e9bf6f300a6d4a16625f3a772a790c7e9ccc89
pratik213 <dahalp30@gmail.com>
2023-03-13 14:19:42 +0545

    Creating models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator,MinValueValidator
 
-# Create your models here.
+PROVINCE_CHOICES=(
+    ('PROVINCE 1','PROVINCE 1'),
+    ('PROVINCE 2','PROVINCE 2'),
+    ('PROVINCE 3','PROVINCE 3'),
+    ('PROVINCE 4','PROVINCE 4'),
+    ('PROVINCE 5','PROVINCE 5'),
+    ('PROVINCE 6','PROVINCE 6'),
+    ('PROVINCE 7','PROVINCE 7'),
+)
+
+class Customer(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    name=models.CharField(max_length=200)
+    locality=models.CharField(max_length=200)
+    city=models.CharField(max_length=200)
+    zipcode=models.IntegerField()
+    state=models.CharField(choices=PROVINCE_CHOICES,max_length=50)
+
+    def __str__(self):
+        return str(self.id)
+        
+    
+
