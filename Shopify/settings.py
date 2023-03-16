1f1f5e1eef8faedf1664b0725d2d8a2ad2fedb0f
pratik213 <dahalp30@gmail.com>
2023-03-16 10:38:36 +0545

    Completed login redirrect

--- Shopify/settings.py
+++ Shopify/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
+LOGIN_REDIRECT_URL='/profile/'
+
--- app/urls.py
+++ app/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('bottomwear/<slug:data>', views.bottomwear, name='bottomweardata'),
     path('bottomwear/', views.bottomwear, name='bottomwear'),
     path('accounts/login', auth_views.LoginView.as_view(template_name="app/login.html",authentication_form=LoginForm), name='login'),
+    path('logout/',auth_views.LogoutView.as_view(next_page='login'),name='logout'),
     path('registration/', views.CustomerRegistration.as_view(), name='customerregistration'),
     path('checkout/', views.checkout, name='checkout'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
