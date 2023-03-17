2304b61f90b9c1156ea34d11972edbe5735d86ea
pratik213 <dahalp30@gmail.com>
2023-03-17 15:48:28 +0545

    Cart remaining

--- app/urls.py
+++ app/urls.py
@@ -8,17 +8,20 @@ from . forms import LoginForm,MyPasswordChangeForm,MyPasswordResetForm,MySetPass
 urlpatterns = [
     path('', views.ProductView.as_view()),
     path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
-    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.show_cart, name='showcart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
+
     path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='app/changepassword.html',form_class=MyPasswordChangeForm,success_url='/passwordchangedone/'), name='changepassword'),
     path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name='app/passwordchangedone.html'), name='changepassworddone'),
     path('password-reset/',auth_views.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
     path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='app/password_reset_done.html'),name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='app/password_reset_confirm.html',form_class=MySetPasswordForm),name='password_reset_confirm'),
     path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name='app/password_reset_complete.html'),name='password_reset_complete'),
+
     path('mobile/<slug:data>', views.mobile, name='mobiledata'),
     path('mobile/', views.mobile, name='mobile'),
     path('laptop/<slug:data>', views.laptop, name='laptopdata'),
--- app/views.py
+++ app/views.py
@@ -33,7 +33,29 @@ class ProductDetailView(View):
 
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    user=request.user
+    product_id=request.GET.get('prod_id')
+    product=Product.objects.get(id=product_id)
+    Cart(user=user,product=product).save()
+    return redirect('/cart')
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user=request.user
+        cart=Cart.objects.filter(user=user)
+        amount=0.00
+        shipping_amount=70.0
+        total_amount=0.0
+        cart_product=[p for p in Cart.objects.all() if p.user==user]
+        if cart_product:
+            for p in cart_product:
+                tempamount=(p.quantity * p.product.discounted_price)
+                amount+=tempamount
+                total_amount=amount+shipping_amount
+            return render(request,'app/addtocart.html',{'carts':cart,'total_amount':total_amount,'amount':amount})
+        else:
+            return render(request,'app/emptycart.html')
+
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
